3c68715f1c6a1590e7392a42a319e4328402a89b
shuvokhan3 <shuvoj360@gmail.com>
2025-12-25 23:45:50 +0600

    First commit

---
+++ dataType.py
@@ -0,0 +1,60 @@
+#int datatype
+age = 3
+print(age)
+age = age + 1
+print(age)
+print(age)
+
+
+
+
+#float datatype
+res = 22.3
+print(type(res))
+
+#complex number
+complex_number = 20+34j
+print(complex_number)
+
+#string datatype
+name = "shuvo"
+print(name)
+
+#Sequence datatype
+
+
+#list datatype (mutable datatype)
+city=["Dhaka", "kulna", "Barshal"]
+value = [1,2,3,4,5]
+print(type(value))
+print(city)
+
+name = ["rahim", "karim"]
+print("First Name",name)
+name[0] = "shuvo"
+print("Change Name", name)
+
+
+
+
+
+#tuple datatype
+numbers = ("A", "B", "C", "D", "E", 2)
+print(numbers)
+print(type(numbers))
+
+#range datatype
+val1 = range(1,20)
+print(*val1)
+
+val3 = range(1,10,2)
+print(*val3)
+
+val4 = range(10)
+print(*val4)
+
+
+
+
+
+
