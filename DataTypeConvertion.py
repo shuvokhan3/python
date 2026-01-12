570d5748106a9830a9911951499360d43484bcb4
shuvokhan3 <shuvoj360@gmail.com>
2026-01-12 15:16:52 +0600

    Data type converstion

---
+++ DataTypeConvertion.py
@@ -0,0 +1,15 @@
+#implicit conversion : when program automatically convert datatype
+
+#explicit conversion : when programmer manually convert datatype
+
+#implicit conversion
+num1 = 23
+num2 = 45.3
+print(type(num1 + num2))
+
+
+#explicit conversion
+value = 12.3
+value2 = 34
+convertInt = int(value)
+print(type(convertInt + value2))
