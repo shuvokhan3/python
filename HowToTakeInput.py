50a96b80af0327500da9a7ba37017596a5eb8ed1
shuvokhan3 <shuvoj360@gmail.com>
2026-01-12 15:18:42 +0600

    Input function

---
+++ HowToTakeInput.py
@@ -0,0 +1,19 @@
+name = input("What is your name?")
+print("My name is " + name)
+
+#take two input number
+# when take input it is by default output as string
+num1 = input("What is your first number?")
+num2 = input("What is your second number?")
+
+#convert this two input string number as int
+num1 = int(num1)
+num2 = int(num2)
+
+#print this two input value
+print(num1 + num2)
+
+
+
+
+
