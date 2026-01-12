db1e5809ff06d54747e82eca8bd9c0f99be1de4f
shuvokhan3 <shuvoj360@gmail.com>
2026-01-12 14:25:39 +0600

    Data type check

---
+++ DatatypeCheck.py
@@ -0,0 +1,17 @@
+number = 12.3
+name = "Shuvo"
+
+value = {
+    "name":"rahim",
+    "number":number
+}
+
+rangeVal = range(1,2,3)
+
+arr = [1,2,3,4,5]
+
+valueData = (1,2,3,4,5)
+
+setval = {1,2,34}
+dataType = type(valueData)
+print(dataType)
