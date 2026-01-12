38f7ccf5f5f0f61f36d844a950a93e1c060914ca
shuvokhan3 <shuvoj360@gmail.com>
2026-01-12 15:18:06 +0600

    Immutable Data type

---
+++ immutableDatatype.py
@@ -0,0 +1,11 @@
+value = 33
+first_location = id(value)
+print(first_location)
+value = 45
+second_location = id(value)
+print(second_location)
+
+#Here it can not modify the current location. it alocate antoher memory address
+
+
+
