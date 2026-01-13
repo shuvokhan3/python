448fd91bf7604249db31692026ae007779f1d002
shuvokhan3 <shuvoj360@gmail.com>
2026-01-13 15:23:53 +0600

    Single Double Three quote string

---
+++ string/singleDoubleQuteString.py
@@ -0,0 +1,15 @@
+
+#single quote use to write shore string
+name = ' shuvo "Khan" '
+print(name)
+
+
+#Double quote use to long string
+lastName = "Raisul"
+print(lastName)
+
+#Three quote
+finalName = '''
+the name of our county is bangldesh
+'''
+print(finalName)
