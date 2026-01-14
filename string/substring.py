1b4ca325bdeac9edcbb748855fe750594f8d44be
shuvokhan3 <shuvoj360@gmail.com>
2026-01-14 13:26:23 +0600

    Work with substring

---
+++ string/substring.py
@@ -0,0 +1,30 @@
+#check if string starts with a substring
+text = "hello world"
+value = text.startswith("hello")
+print(value)
+
+#checking ending postion
+print(text.endswith("world"))
+
+#Find the postion of the substring
+print(text.find("e"))
+
+#count occurence of string
+print(text.count("l"))
+
+#check if char alphanumaric or not
+print(text.isalnum())
+
+#check if all characters are alphabetic
+print(text.isalpha())
+
+#check digit extis
+print(text.isdigit())
+
+#chek if the string containts space
+print(text.isspace())
+
+#check title or not
+print(text.istitle())
+
+
