0b0f88053452e8be822f77902ce034262a4d27cf
shuvokhan3 <shuvoj360@gmail.com>
2026-01-15 12:45:10 +0600

    Basic Operation And Type Convertion

---
+++ numberAndMath/basicOperationAndTypeConvertion.py
@@ -0,0 +1,18 @@
+number1 = 34
+number2 = 2
+print(number1 + number2)
+print("Addition : " ,number2 + number1)
+print("Subtraction : ", number1 - number2)
+print("Multiplication: ", number1 * number2)
+print("Division: ", number1 / number2)
+print("Modulus: ", number1 % number2)
+print("exponent: ", number1 ** number2)
+
+
+#type conversion
+val = 10
+val2 = 3.1415
+print("Int to float", float(val))
+print("Float to int", int(val2))
+print("Int to complex number", complex(val))
+
