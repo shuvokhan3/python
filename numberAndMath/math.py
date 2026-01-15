cef715e29bb946c21a24c6e8513d2796e7cc811e
shuvokhan3 <shuvoj360@gmail.com>
2026-01-15 13:02:30 +0600

    Math Operation

---
+++ numberAndMath/math.py
@@ -0,0 +1,35 @@
+import math
+#square root
+print(math.sqrt(420))
+
+#power
+print(math.pow(4,2))
+
+#Trigonometric function
+print(math.sin(math.radians(90)))
+print(math.cos(math.radians(90)))
+
+#Logarithmic function
+print(math.log2(10))
+
+#Factorial
+print(math.factorial(6))
+
+#Absolute value
+print(math.fabs(-44))
+
+#Ceil
+print(math.ceil(3))
+
+#floor
+print(math.floor(3.876987))
+
+#constant
+print(math.pi)
+
+#GCD
+num1 = 34
+num2 = 78
+print(math.gcd(num1, num2))
+print(math.lcm(num1, num2))
+
