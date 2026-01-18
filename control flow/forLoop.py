cf0bae96b9a0bcec8650078b8b472b5718fa43bd
shuvokhan3 <shuvoj360@gmail.com>
2026-01-18 15:04:05 +0600

    for Loop

---
+++ control flow/forLoop.py
@@ -0,0 +1,16 @@
+data = [1,2,3,4]
+for i in data:
+    print(i)
+
+for item in range(5):
+    print(item)
+
+marks = {
+    'phy' : 344,
+    'math' : 456
+}
+for key, value in marks.items():
+    print(key, value)
+
+
+
