5b4d417e1a56f07d51f97cdfded672342cf767fc
shuvokhan3 <shuvoj360@gmail.com>
2026-01-27 23:47:33 +0600

    While loop

--- control flow/whileLoop.py
+++ control flow/whileLoop.py
@@ -1,8 +1,17 @@
 value = [1,2,3,45]
 i = 0
+
 while i < len(value):
     print(value[i])
     i = i + 1
+
+
+data = ["a","b","c","d","e","f"]
+ind = 0
+while(ind < len(data)):
+    print(data[ind])
+    ind = ind + 1
+
 
 
 
