5bd4621a2d9214a7c46dd73370347abda3b36a21
shuvokhan3 <shuvoj360@gmail.com>
2026-02-04 22:00:52 +0600

    file dir

--- fileDirectory/makedir.py
+++ fileDirectory/makedir.py
@@ -1,9 +1,6 @@
 import os
-
 os.makedirs("mkdir", exist_ok=True)
-
-# with open("mkdir/test.txt", "w") as file:
-#     file.write("Bangladesh is an independent country")
-
+with open("mkdir/test.txt", "w") as file:
+    file.write("Bangladesh is an independent country")
 file_list = os.listdir("mkdir")
-print(type(file_list))
+print(file_list)
