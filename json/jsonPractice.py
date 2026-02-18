6f94a3556153868f56907eb8b18d99412e5ef380
shuvokhan3 <shuvoj360@gmail.com>
2026-02-18 23:57:06 +0600

    Convert json to python obj

--- json/jsonPractice.py
+++ json/jsonPractice.py
@@ -10,14 +10,20 @@ import json
 import sys
 
 #convert python obj to json string
-person = {
-    "name": "shuvo",
-    "age": 22,
-    "city": "dhaka"
-}
-personConvert = json.dump(person, sys.stdout)
-print(personConvert)
+# person = {
+#     "name": "shuvo",
+#     "age": 22,
+#     "city": "dhaka"
+# }
+# personConvert = json.dump(person, sys.stdout)
+# print(personConvert)
 
 
+#convert json to python object
+personJson = '{"name": "John", "age": 5}'
+personObj = json.loads(personJson)
+print(personObj)
+
+print("first name is :", personObj['name'])
 
 
