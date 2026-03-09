28d4c47e42626006dc6c9c6f019bacf3b7f720b8
shuvokhan3 <shuvoj360@gmail.com>
2026-03-09 23:40:33 +0600

    inharitance

---
+++ OOP/inharitancethree.py
@@ -0,0 +1,16 @@
+class Father:
+    x = 34
+    y = 54
+
+    #without creating any object this class able to access this method
+    @staticmethod
+    def add():
+        print(Father.x + Father.y)
+
+
+        
+class Son(Father):
+    pass
+
+
+Son.add()
