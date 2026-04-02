b4c4e531588398b09328c214dd9e28096e769ca9
shuvokhan3 <shuvoj360@gmail.com>
2026-04-02 23:28:31 +0600

    abstraction

---
+++ OOP/abstraction.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class Student(ABC):
+    @abstractmethod
+    def address(self):
+        pass
+    @abstractmethod
+    def name(self):
+        pass
+    @abstractmethod
+    def score(self):
+        pass
+
+class Student2(Student):
+    def address(self):
+        print("Nazirpur , Dhirjan , ruhitalabunia")
+
+    def name(self):
+        print("raisul islam")
+
+    def score(self):
+        print("33")
+
+val = Student2()
+val.address()
+val.name()
+val.score()
