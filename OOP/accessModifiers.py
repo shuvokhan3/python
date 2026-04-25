d86e394050b3261d8cced41e582054bb08c9adb9
shuvokhan3 <shuvoj360@gmail.com>
2026-04-25 23:33:22 +0600

    protected access modifier, private access modifier

--- OOP/accessModifiers.py
+++ OOP/accessModifiers.py
@@ -6,7 +6,32 @@ class car :
     def Display(self):
         print(self.brand)
 
+class bus:
+    #protected access modifier
+    _brand = "Volvo"
 
+    def Display(self):
+        print(self._brand)
+
+class bike:
+
+    #private access modifier
+    __brand = "Honda"
+
+    def Display(self):
+        print(self.__brand)
+
+    
+
+    
+#public access modifier
 obj = car()
 obj.Display()
 
+#protected access modifier
+obj1 = bike()
+obj1.Display()
+
+#private access modifier
+obj2 = bus()
+obj2.Display()
