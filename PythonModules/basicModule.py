9b9df3deed63ad12b62be71ef48d828897eca6bb
shuvokhan3 <shuvoj360@gmail.com>
2026-06-07 23:50:24 +0600

    what is python module system how it work and try some module

---
+++ PythonModules/basicModule.py
@@ -0,0 +1,22 @@
+# math module :provide mathamatical operations
+
+import math
+
+print(math.sqrt(13))
+print(math.pi)
+print(math.cos(0))
+print(math.sin(0))
+print(math.tan(0))
+print(math.log(10))
+
+
+# random module : provide random number generation
+print("\n")
+import random
+print(random.random())  # random float between 0 and 1
+print(random.randint(1, 10))  # random integer between 1 and 10
+
+#os module : provide functions for interacting with the operating system
+import os
+print(os.getcwd())  # get current working directory
+print(os.listdir())  # list files in current directory  
