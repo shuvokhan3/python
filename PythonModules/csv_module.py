dbfbfb9f4aa9913d4b7bb6a3a0408b8825b0cd7c
shuvokhan3 <shuvoj360@gmail.com>
2026-06-13 23:34:28 +0600

    csv module

---
+++ PythonModules/csv_module.py
@@ -0,0 +1,12 @@
+import csv
+
+with open('data.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Name', 'Age', 'City'])
+    writer.writerow(['Alice', 30, 'New York'])
+    writer.writerow(['Bob', 25, 'Los Angeles'])
+
+with open('data.csv', mode='r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row) 
