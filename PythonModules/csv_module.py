9465cc33da68266292402b6dabb7dc0d116c9a5b
shuvokhan3 <shuvoj360@gmail.com>
2026-06-17 17:59:07 +0600

    PythonModules/csv_module.py

--- PythonModules/csv_module.py
+++ PythonModules/csv_module.py
@@ -6,7 +6,7 @@ with open('data.csv', mode='w', newline='') as file:
     writer.writerow(['Alice', 30, 'New York'])
     writer.writerow(['Bob', 25, 'Los Angeles'])
 
-    
+
 
 with open('data.csv', mode='r') as file:
     reader = csv.reader(file)
