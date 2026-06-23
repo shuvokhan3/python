a21dae7ab08d1894e6983d6048178193f6894e34
shuvokhan3 <shuvoj360@gmail.com>
2026-06-23 22:39:08 +0600

    module system

---
+++ PythonModules/singlefilemodule.py
@@ -0,0 +1,22 @@
+from userdefinemodule import sub
+from userdefinemodule import add
+from userdefinemodule import x, y
+from userdefinemodule import message
+
+
+##access function
+print(sub(19, 3))
+print(add(2,3))
+
+##access variable
+print(x)
+print(y)
+
+##access class from custome module and print class 
+obj = message(20,2)
+print(obj)
+
+
+
+
+
---
+++ PythonModules/userdefinemodule.py
@@ -0,0 +1,16 @@
+def add (a , b):
+    return a + b
+
+def sub(a , b):
+    return a * b
+
+x = 4
+y = 7
+
+
+class message:
+    def __init__(self):
+        print("Hello world")
+
+    def divided(self, x,y):
+        return x / y
